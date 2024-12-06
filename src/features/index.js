a1d577bc276fba800ecd633722741648bba4b353
mowaisfarid <owais@globalresourcesca.com>
2024-12-06 15:58:45 +0500

    chore: project init

---
+++ src/features/index.js
@@ -0,0 +1,8 @@
+// dashboard screen
+export * from "./dashboard";
+
+// auth screens
+export { LoginScreen, SignupScreen } from "./auth";
+
+// settings screens
+export { Settings } from "./settings/";
---
+++ src/navigation/index.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Dashboard, Settings, LoginScreen, SignupScreen } from "@/features";
+
+const Stack = createNativeStackNavigator();
+const RootContainer = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootContainer;
+
+const styles = StyleSheet.create({});
